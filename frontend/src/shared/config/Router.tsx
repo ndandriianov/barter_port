0c43604c103595637ea6ba0b628e971eacef8736
nick <11andriianovnikita@gmail.com>
2026-02-25 17:49:22 +0300

    добавил ссылку на профиль для тестирования токена
    надо сделать нормальный header
    
    надо сделать сохранение access в localstorage и проверить refresh

--- frontend/src/shared/config/Router.tsx
+++ frontend/src/shared/config/Router.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import LoginPage from "@/pages/auth/LoginPage";
 import ProfilePage from "@/pages/profile/ProfilePage";
 import RegisterPage from "@/pages/auth/RegisterPage.tsx";
@@ -7,6 +7,8 @@ import VerifyEmailPage from "@/pages/auth/VerifyEmailPage.tsx";
 function AppRouter() {
   return (
     <BrowserRouter>
+      <Link to={"/profile"}>Profile</Link>
+
       <Routes>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/profile" element={<ProfilePage/>}/>
