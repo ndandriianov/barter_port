d057369f40c2590940986a6f6a59d46cd04f01e1
nick <11andriianovnikita@gmail.com>
2026-02-26 16:57:15 +0300

    сделал logout в ProfilePage.tsx таким же, как и в Header.tsx

--- frontend/src/pages/profile/ProfilePage.tsx
+++ frontend/src/pages/profile/ProfilePage.tsx
@@ -1,16 +1,27 @@
 import authApi from "@/features/auth/api/authApi";
+import {useAppDispatch} from "@/hooks/redux.ts";
+import {logout} from "@/features/auth/model/authSlice.ts";
+import {useNavigate} from "react-router-dom";
 
 function ProfilePage() {
   const { data, isLoading, refetch } = authApi.useMeQuery();
-  const [logout] = authApi.useLogoutMutation();
+  const [apiLogout] = authApi.useLogoutMutation();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   if (isLoading) return <div>Loading...</div>;
   if (!data) return <div>Not authorized</div>;
 
+  async function handleLogout() {
+    await apiLogout();
+    dispatch(logout());
+    navigate("/login");
+  }
+
   return (
     <div>
       <div>UserID: {data.userId}</div>
-      <button onClick={() => logout()}>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
       <button onClick={() => refetch()}>Refetch</button>
     </div>
   );
