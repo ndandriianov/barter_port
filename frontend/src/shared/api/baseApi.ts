37cddf75f9c42652891376cb615fc2f7325bc25a
nick <11andriianovnikita@gmail.com>
2026-02-26 14:39:42 +0300

    исправил баг из-за которого токен не сохранялся

--- frontend/src/shared/api/baseApi.ts
+++ frontend/src/shared/api/baseApi.ts
@@ -4,11 +4,16 @@ import type {FetchArgs, FetchBaseQueryError} from "@reduxjs/toolkit/query";
 import {type RootState} from "@/app/store/store";
 import {setCredentials, logout} from "@/features/auth/model/authSlice";
 
+type RefreshResponse = {
+  access_token: string;
+};
+
 const rawBaseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:8080",
   credentials: "include", // для refresh cookie
   prepareHeaders: (headers, {getState}) => {
     const token = (getState() as RootState).auth.accessToken;
+    console.log("token", token);
     if (token) {
       headers.set("Authorization", `Bearer ${token}`);
     }
@@ -19,20 +24,24 @@ const rawBaseQuery = fetchBaseQuery({
 export const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError>
   = async (args, api, extraOptions) => {
 
+  console.log("попытка отправить запрос")
   let result = await rawBaseQuery(args, api, extraOptions);
 
   if (result.error && result.error.status === 401) {
     // пробуем refresh
+    console.log("попытка обновить токен")
     const refreshResult = await rawBaseQuery(
       {url: "/auth/refresh", method: "POST"},
       api,
       extraOptions
     );
 
+    console.log("токен обновлен", refreshResult)
     if (refreshResult.data) {
-      const {accessToken} = refreshResult.data as { accessToken: string };
-      api.dispatch(setCredentials(accessToken));
+      const {access_token} = refreshResult.data as RefreshResponse;
+      api.dispatch(setCredentials(access_token));
 
+      console.log("повторная попытка отправить запрос с новым токеном")
       result = await rawBaseQuery(args, api, extraOptions);
     } else {
       api.dispatch(logout());
