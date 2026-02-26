5d044ae0e3834f124189ef673459cd2e78bbf6dd
nick <11andriianovnikita@gmail.com>
2026-02-26 18:53:12 +0300

    при logout кэшированное локальное состояние стирается
    logout вынесен в функцию performLogout

--- frontend/src/widgets/Header.tsx
+++ frontend/src/widgets/Header.tsx
@@ -1,16 +1,13 @@
 import {Link, useNavigate} from "react-router-dom";
-import authApi from "@/features/auth/api/authApi.ts";
 import {useAppDispatch} from "@/hooks/redux.ts";
-import {logout} from "@/features/auth/model/authSlice.ts";
+import {performLogout} from "@/features/auth/model/logoutThunk.ts";
 
 function Header() {
-  const [apiLogout] = authApi.useLogoutMutation()
   const navigate = useNavigate()
   const dispatch = useAppDispatch();
 
   async function handleLogout() {
-    await apiLogout();
-    dispatch(logout());
+    await dispatch(performLogout());
     navigate("/login");
   }
 
