c9b38e478cbbddfbe211eb2fa0cd3e69ea137aef
nick <11andriianovnikita@gmail.com>
2026-03-06 17:50:24 +0300

    поправил порт в swagger, теперь запросы работают как надо

--- backend/docs/auth/docs.go
+++ backend/docs/auth/docs.go
@@ -360,7 +360,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0.0",
-	Host:             "localhost:8081",
+	Host:             "localhost:80",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
 	Title:            "Barter Port API",
--- backend/docs/items/docs.go
+++ backend/docs/items/docs.go
@@ -255,7 +255,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0.0",
-	Host:             "",
+	Host:             "localhost:80",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
 	Title:            "Barter Port API",
