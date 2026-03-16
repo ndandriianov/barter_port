23c98e6f200cd54792e271ed3f178f77c006c70a
nick <11andriianovnikita@gmail.com>
2026-03-17 01:37:42 +0300

    создал базу данных в контейнере postgres для сервиса users
    описал основные http методы этого сервиса
    
    начал писать service

--- backend/init.sql
+++ backend/init.sql
@@ -40,3 +40,53 @@ CREATE TABLE items
 
     FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE DATABASE users_db;
+
+\connect users_db;
+
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    name       TEXT        NOT NULL,
+    bio        TEXT
+);
+
+CREATE TABLE deleted_users
+(
+    id         UUID PRIMARY KEY,
+    deleted_at TIMESTAMPTZ NOT NULL
+)
+
+-- CREATE TABLE offers
+-- (
+--     id          UUID PRIMARY KEY,
+--     item_id     UUID,
+--     deal_id     UUID        NOT NULL,
+--     name        TEXT        NOT NULL,
+--     description TEXT,
+--     created_at  TIMESTAMPTZ NOT NULL,
+--     receiver_id UUID,
+--     sender_id   UUID,
+--
+--     FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE SET NULL,
+--     FOREIGN KEY (receiver_id) REFERENCES users (id) ON DELETE SET NULL,
+--     FOREIGN KEY (sender_id) REFERENCES users (id) ON DELETE SET NULL
+-- );
+--
+-- CREATE TABLE deals
+-- (
+--     id     UUID PRIMARY KEY,
+--     status TEXT NOT NULL,
+--
+--     CONSTRAINT deals_status_check CHECK (
+--         status IN (
+--                    'SEARCHING_FOR_PARTICIPANTS',
+--                    'DISCUSSION',
+--                    'APPROVED',
+--                    'COMPLETED',
+--                    'CANCELLED',
+--                    'FAILED'
+--             )
+--         )
+-- );
