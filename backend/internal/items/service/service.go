9c5a47f458feb8bf3ea655db28ef6464fb1cd5d8
nick <11andriianovnikita@gmail.com>
2026-03-16 20:19:09 +0300

    поправил проблемы с получением items

--- backend/internal/items/service/service.go
+++ backend/internal/items/service/service.go
@@ -79,7 +79,7 @@ func (s *ItemService) GetItems(
 	sortType model.SortType,
 	cursor *model.UniversalCursor,
 	limit int,
-) ([]model.Item, model.UniversalCursor, error) {
+) ([]model.Item, *model.UniversalCursor, error) {
 
 	log := logger.LogFrom(ctx, s.fallbackLogger)
 
@@ -97,10 +97,15 @@ func (s *ItemService) GetItems(
 
 		items, newCursor, err := s.repo.GetItemsOrderByTime(ctx, timeCursor, limit)
 		if err != nil {
-			return nil, model.UniversalCursor{}, err
+			return nil, nil, err
 		}
 
-		return items, *newCursor.ToUniversalCursor(), nil
+		var universalCursor *model.UniversalCursor
+		if newCursor != nil {
+			universalCursor = newCursor.ToUniversalCursor()
+		}
+
+		return items, universalCursor, nil
 
 	case model.ByPopularity:
 		var popularityCursor *model.PopularityCursor
@@ -109,19 +114,24 @@ func (s *ItemService) GetItems(
 		if cursor != nil {
 			popularityCursor, err = cursor.ToPopularityCursor()
 		}
-		if err != nil {
+		if err != nil || popularityCursor == nil {
 			log.Debug("popularity cursor is not specified, starting from the beginning", slog.Any("error", err))
 		}
 
 		items, newCursor, err := s.repo.GetItemsOrderByPopularity(ctx, popularityCursor, limit)
 		if err != nil {
-			return nil, model.UniversalCursor{}, err
+			return nil, nil, err
 		}
 
-		return items, *newCursor.ToUniversalCursor(), nil
+		var universalCursor *model.UniversalCursor
+		if newCursor != nil {
+			universalCursor = newCursor.ToUniversalCursor()
+		}
+
+		return items, universalCursor, nil
 
 	default:
-		return nil, model.UniversalCursor{}, fmt.Errorf("invalid sort type: %v", sortType)
+		return nil, nil, fmt.Errorf("invalid sort type: %v", sortType)
 	}
 }
 
