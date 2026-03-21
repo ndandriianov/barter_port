b6a4540167fe3ccebc8a325134e31b53ff8a316d
nick <11andriianovnikita@gmail.com>
2026-03-21 16:10:00 +0300

    пользователь создается в users, цепочка через kafka работает

---
+++ backend/cmd/users/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"barter-port/internal/libs/bootstrap"
+	"barter-port/internal/users/app"
+	"errors"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	cfg, err := loadConfig()
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	usersApp, err := app.NewApp(cfg)
+	if err != nil {
+		log.Fatalf("new app: %v", err)
+	}
+
+	err = usersApp.Run()
+	if err != nil {
+		log.Fatalf("run: %v", err)
+	}
+}
+
+func loadConfig() (bootstrap.Config, error) {
+	cfg, err := bootstrap.LoadConfig(bootstrap.ConfigOptions{
+		CommonPath:  os.Getenv("CONFIG_COMMON"),
+		ServicePath: os.Getenv("CONFIG_SERVICE"),
+		AppEnv:      os.Getenv("APP_ENV"),
+	})
+	if err != nil {
+		return bootstrap.Config{}, errors.New("failed to load config: " + err.Error())
+	}
+
+	return cfg, nil
+}
--- backend/internal/libs/kafkax/message_reader.go
+++ backend/internal/libs/kafkax/message_reader.go
@@ -2,7 +2,7 @@ package kafkax
 
 import kafkago "github.com/segmentio/kafka-go"
 
-func NewReaderMessageReader(brokers []string, topic string, groupId string) *kafkago.Reader {
+func NewMessageReader(brokers []string, topic string, groupId string) *kafkago.Reader {
 	return kafkago.NewReader(kafkago.ReaderConfig{
 		Brokers: brokers,
 		Topic:   topic,
