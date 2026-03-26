2493435b3966ca0f69d9c4dee7fb928cc61df958
nick <11andriianovnikita@gmail.com>
2026-03-26 14:23:04 +0300

    вынес общую логику consumers в kafkax

---
+++ backend/internal/libs/kafkax/inbox_consumer.go
@@ -0,0 +1,97 @@
+package kafkax
+
+import (
+	"barter-port/internal/libs/errorx"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log/slog"
+	"time"
+
+	kafkago "github.com/segmentio/kafka-go"
+)
+
+type InboxConsumer[T any] struct {
+	log          *slog.Logger
+	reader       messageReader
+	pollInterval time.Duration
+}
+
+type messageReader interface {
+	FetchMessage(context.Context) (kafkago.Message, error)
+	CommitMessages(context.Context, ...kafkago.Message) error
+	Close() error
+}
+
+var ErrDuplicate = errors.New("duplicate message")
+
+func NewInboxConsumer[T any]() *InboxConsumer[T] {
+	return &InboxConsumer[T]{}
+}
+
+func (c *InboxConsumer[T]) Run(ctx context.Context, processMessage func(context.Context, T) error) error {
+	defer func() {
+		if err := c.reader.Close(); err != nil {
+			c.log.Error("failed to close Kafka consumer", slog.Any("error", err))
+		}
+	}()
+
+	for {
+		if ctx.Err() != nil {
+			return nil
+		}
+
+		err := c.consumeMessage(ctx, processMessage)
+		if err == nil {
+			continue
+		}
+
+		if errorx.IsShutdownError(ctx, err) {
+			return nil
+		}
+
+		c.log.Error("failed to consume message", "error", err)
+
+		select {
+		case <-time.After(c.pollInterval):
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (c *InboxConsumer[T]) consumeMessage(ctx context.Context, processMessage func(context.Context, T) error) error {
+	rawMessage, err := c.reader.FetchMessage(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to fetch message: %w", err)
+	}
+
+	var message T
+	err = json.Unmarshal(rawMessage.Value, &message)
+	if err != nil {
+		if commitErr := c.CommitMessage(ctx, rawMessage); commitErr != nil {
+			return fmt.Errorf("failed to unmarshal message id=%s: %w; additionally: %w",
+				string(rawMessage.Key), err, commitErr,
+			)
+		}
+		return fmt.Errorf("failed to unmarshal message id: %s: %w", string(rawMessage.Key), err)
+	}
+
+	err = processMessage(ctx, message)
+	if err != nil {
+		if errors.Is(err, ErrDuplicate) {
+			return c.CommitMessage(ctx, rawMessage)
+		}
+		return fmt.Errorf("failed to process message: %w", err)
+	}
+
+	return c.CommitMessage(ctx, rawMessage)
+}
+
+func (c *InboxConsumer[T]) CommitMessage(ctx context.Context, rawMessage kafkago.Message) error {
+	if commitErr := c.reader.CommitMessages(ctx, rawMessage); commitErr != nil {
+		return fmt.Errorf("commit message id: %s: %w", string(rawMessage.Key), commitErr)
+	}
+	return nil
+}
--- backend/internal/users/infrastructure/kafka/consumer/user_creation.go
+++ backend/internal/users/infrastructure/kafka/consumer/user_creation.go
@@ -3,120 +3,49 @@ package consumer
 import (
 	authusers "barter-port/internal/contracts/kafka/messages/auth-users"
 	"barter-port/internal/libs/db"
-	"barter-port/internal/libs/errorx"
+	"barter-port/internal/libs/kafkax"
 	"barter-port/internal/users/infrastructure/repository/inbox"
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"log/slog"
-	"time"
 
-	kafkago "github.com/segmentio/kafka-go"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type UserCreationInboxConsumer struct {
-	log          *slog.Logger
-	reader       messageReader
-	db           db.DB
-	inboxRepo    inboxWriter
-	pollInterval time.Duration
-}
-
-type messageReader interface {
-	FetchMessage(context.Context) (kafkago.Message, error)
-	CommitMessages(context.Context, ...kafkago.Message) error
-	Close() error
+	db        *pgxpool.Pool
+	inboxRepo inboxWriter
+	consumer  *kafkax.InboxConsumer[authusers.UserCreationMessage]
 }
 
 type inboxWriter interface {
 	WriteUserCreationMessage(context.Context, db.DB, authusers.UserCreationMessage) error
 }
 
-type Params struct {
-	Log          *slog.Logger
-	Reader       messageReader
-	DB           db.DB
-	InboxRepo    inboxWriter
-	PollInterval time.Duration
-}
-
-func NewUserCreationInboxConsumer(params Params) *UserCreationInboxConsumer {
-	if params.PollInterval <= 0 {
-		params.PollInterval = time.Second * 5
-	}
-
+func NewUserCreationInboxConsumer(
+	db *pgxpool.Pool,
+	inboxRepo inboxWriter,
+	consumer *kafkax.InboxConsumer[authusers.UserCreationMessage],
+) *UserCreationInboxConsumer {
 	return &UserCreationInboxConsumer{
-		log:          params.Log,
-		reader:       params.Reader,
-		db:           params.DB,
-		inboxRepo:    params.InboxRepo,
-		pollInterval: params.PollInterval,
+		db:        db,
+		inboxRepo: inboxRepo,
+		consumer:  consumer,
 	}
 }
 
 func (c *UserCreationInboxConsumer) Run(ctx context.Context) error {
-	defer func() {
-		if err := c.reader.Close(); err != nil {
-			c.log.Error("failed to close Kafka consumer", slog.Any("error", err))
-		}
-	}()
-
-	for {
-		if ctx.Err() != nil {
-			return nil
-		}
-
-		err := c.consumeMessage(ctx)
-		if err == nil {
-			continue
-		}
-
-		if errorx.IsShutdownError(ctx, err) {
-			return nil
-		}
-
-		c.log.Error("failed to consume message", "error", err)
-
-		select {
-		case <-time.After(c.pollInterval):
-		case <-ctx.Done():
-			return nil
-		}
-	}
+	return c.consumer.Run(ctx, c.consumeMessage)
 }
 
-func (c *UserCreationInboxConsumer) consumeMessage(ctx context.Context) error {
-	rawMessage, err := c.reader.FetchMessage(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to fetch message: %w", err)
-	}
-
-	var message authusers.UserCreationMessage
-	err = json.Unmarshal(rawMessage.Value, &message)
-	if err != nil {
-		if commitErr := c.commitMessage(ctx, rawMessage); commitErr != nil {
-			return fmt.Errorf("failed to unmarshal message id=%s: %w; additionally: %w",
-				string(rawMessage.Key), err, commitErr,
-			)
-		}
-		return fmt.Errorf("failed to unmarshal message id: %s: %w", string(rawMessage.Key), err)
-	}
-
-	err = c.inboxRepo.WriteUserCreationMessage(ctx, c.db, message)
+func (c *UserCreationInboxConsumer) consumeMessage(ctx context.Context, message authusers.UserCreationMessage) error {
+	err := c.inboxRepo.WriteUserCreationMessage(ctx, c.db, message)
 	if err != nil {
 		if errors.Is(err, inbox.ErrUCEventAlreadyExists) {
-			return c.commitMessage(ctx, rawMessage)
+			return kafkax.ErrDuplicate
 		}
 		return fmt.Errorf("failed to write user creation message to inbox: %w", err)
 	}
 
-	return c.commitMessage(ctx, rawMessage)
-}
-
-func (c *UserCreationInboxConsumer) commitMessage(ctx context.Context, rawMessage kafkago.Message) error {
-	if commitErr := c.reader.CommitMessages(ctx, rawMessage); commitErr != nil {
-		return fmt.Errorf("commit message id: %s: %w", string(rawMessage.Key), commitErr)
-	}
 	return nil
 }
