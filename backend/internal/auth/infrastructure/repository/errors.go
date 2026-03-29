8a0d07dfd9239164b2888c444202f9765e819e84
nick <11andriianovnikita@gmail.com>
2026-03-29 12:23:05 +0300

    исправил ошибку

--- backend/internal/auth/infrastructure/repository/errors.go
+++ backend/internal/auth/infrastructure/repository/errors.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/jackc/pgconn"
 	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 // UniqueError represents a uniqueness constraint violation error.
