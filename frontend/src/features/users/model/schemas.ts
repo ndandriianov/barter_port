9209a073d9386f24e7edc651176db6de03ca5113
nick <11andriianovnikita@gmail.com>
2026-04-12 18:24:12 +0300

    можно загружать фото для аватара

--- frontend/src/features/users/model/schemas.ts
+++ frontend/src/features/users/model/schemas.ts
@@ -4,6 +4,7 @@ export const userSchema = z.object({
   id: z.string(),
   name: z.string().optional(),
   bio: z.string().optional(),
+  avatarUrl: z.string().optional(),
 });
 
 export const meSchema = userSchema.extend({
